95c62019d42815cc044b2ecbaf9501abf28117ee
jsm <sang93423@gmail.com>
2018-10-31 16:46:03 +0900

    django 2버전대에 바꾸어서 로그인뷰 생성, namspace지정

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import auth_login, profile
 from django.urls import path
 
+app_name = "accounts"
 urlpatterns = [
     path('login/', auth_login, name='auth_login'),
     path('profile/', profile, name='profile'),
