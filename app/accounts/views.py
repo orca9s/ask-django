f1702e1b859e843265d563dc7197a0d953a66199
jsm <sang93423@gmail.com>
2018-11-08 23:29:42 +0900

    profile뷰에 @login_required 데코레이터 사용

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,23 +1,25 @@
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
-# Create your views here.
+# def login_view(request):
+#     """
+#     path('login/', views.login_view, name='login'), 를 받아 처리하는 view
+#
+#     django.contrib.auth 앱을 사용하여 loginview를 대체하여 이 메서드는 사용하지 않음
+#     """
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#
+#         user = authenticate(request, username=username, passwor=password)
+#         login(request, user)
+#
+#         return redirect('accounts:profile')
+#     else:
+#         return render(request, 'accounts/login.html')
 
-
-def auth_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-        login(request, user)
-
-        return redirect('accounts:profile')
-    else:
-        return render(request, 'login.html')
-
-
+@login_required
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'accounts/profile.html')
